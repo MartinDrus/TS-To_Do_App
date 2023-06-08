f00996e6716f40dee26aa9f11af136da6ade38ca
Martin <martindrus@outlook.de>
2023-06-08 13:54:51 +0200

    Implemented a task and its type

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import './style.css';
+import { v4 as uuidV4 } from 'uuid';
+
+type Task = {
+  id: string,
+  title: string,
+  completed: boolean,
+  createdAt: Date
+}
+
 
 const appDiv = document.querySelector<HTMLDivElement>('#app')!;
+
+// Create the list element
+const list = document.createElement('ul');
 
 // Create the form element
 const form = document.createElement('form');
@@ -20,11 +32,28 @@ form.appendChild(addButton);
 // Add a submit event listener to the form
 form.addEventListener('submit', (e) => {
   e.preventDefault(); // Prevent the default form submission behavior
-  const task = input.value; // Get the value from the input field
-  // Do something with the task, like adding it to a list or performing an action
-  console.log(`New task: ${task}`);
+
+  // Check if the input value is empty or null
+  if (input?.value == "" || input?.value == null) {
+    return; // If the input value is empty or null, exit the function or block of code
+  }
+
+  const newTask = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+
+  addListItem(newTask);
+
   input.value = ''; // Clear the input field
 });
 
 // Append the form to the app container
 appDiv.appendChild(form);
+
+
+function addListItem(task: Task) {
+
+}
