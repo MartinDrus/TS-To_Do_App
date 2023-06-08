da3c060affbcb11fcbea07e6bcd8a94acb8fa0a8
Martin <martindrus@outlook.de>
2023-06-08 15:13:44 +0200

    Added localStorage functionality

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './style.css';
+import './style.scss';
 import { v4 as uuidV4 } from 'uuid';
 
 type Task = {
@@ -8,11 +8,14 @@ type Task = {
   createdAt: Date
 }
 
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem)
 
 const appDiv = document.querySelector<HTMLDivElement>('#app')!;
 
 // Create the list element
 const list = document.createElement('ul');
+appDiv.appendChild(list);
 
 // Create the form element
 const form = document.createElement('form');
@@ -38,22 +41,51 @@ form.addEventListener('submit', (e) => {
     return; // If the input value is empty or null, exit the function or block of code
   }
 
-  const newTask = {
-    id: uuidV4(),
-    title: input.value,
-    completed: false,
-    createdAt: new Date()
-  }
+  // Create a new task object
+  const newTask: Task = {
+    id: uuidV4(), // Generate a unique ID using the uuid library (assuming it's imported and available)
+    title: input.value, // Set the title of the task to the value of the input field
+    completed: false, // Set the initial completion status of the task to false
+    createdAt: new Date() // Set the creation date of the task to the current date and time
+  };
 
+  tasks.push(newTask);
+
+  // Add the new task to the task list or perform any other necessary logic
   addListItem(newTask);
 
-  input.value = ''; // Clear the input field
+  // Reset the input field value to an empty string
+  input.value = "";
 });
+
+
 
 // Append the form to the app container
 appDiv.appendChild(form);
 
-
 function addListItem(task: Task) {
 
+  const item = document.createElement("li");
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", ()=> {
+    task.completed = checkbox.checked;
+    saveTasks()
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS")
+  if(taskJSON == null) return []
+  return JSON.parse(taskJSON);
 }
